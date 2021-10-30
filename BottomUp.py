6cf6a5f308ff32bbb5eb26bb8a9cc99cfb8b7de2
Yeshu Wu <y498wu@uwaterloo.ca>
2021-10-29 22:28:40 -0400

    remove

--- BottomUp.py
+++
@@ -1,9 +0,0 @@
-def fib(n):
-    dict = {}
-    for k in range(1, n+1):
-        if k <= 2:
-           f = 1
-        else:
-            f = dict[k-1] + dict[k-2]
-        dict[k] = f
-    return dict[n]
