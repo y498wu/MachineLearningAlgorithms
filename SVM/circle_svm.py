51b8c8ccbb466635f9b425bbf11ec553cd811a1f
Yeshu Wu <y498wu@uwaterloo.ca>
2023-04-03 02:46:22 -0400

    change the inner and outer circles of points into stars

--- SVM/circle_svm.py
+++ SVM/circle_svm.py
@@ -7,5 +7,5 @@ from sklearn import svm
 circle_X, circle_y = datasets.make_circles(n_samples=300, noise=0.05)
 
 # show raw non-linear data
-plt.scatter(circle_X[:, 0], circle_X[:, 1], c=circle_y, marker='.')
+plt.scatter(circle_X[:, 0], circle_X[:, 1], c=circle_y, marker='*')
 plt.show()
