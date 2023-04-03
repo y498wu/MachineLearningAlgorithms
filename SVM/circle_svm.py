5da9a4cb1c31aaacd0bdc43d510b9015bcb5cb11
Yeshu Wu <y498wu@uwaterloo.ca>
2023-04-03 02:09:16 -0400

    draw and show a random circle using sklearn datasets

---
+++ SVM/circle_svm.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets
+from sklearn import svm
+
+# non-linear data
+circle_X, circle_y = datasets.make_circles(n_samples=300, noise=0.05)
+
+# show raw non-linear data
+plt.scatter(circle_X[:, 0], circle_X[:, 1], c=circle_y, marker='.')
+plt.show()
