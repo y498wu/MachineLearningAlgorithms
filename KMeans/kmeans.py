509ef77a9803a3f4f3f22e43398821e08f8564b5
Yeshu Wu <y498wu@uwaterloo.ca>
2023-04-27 23:45:33 -0400

    apply sklearn kmeans

--- KMeans/kmeans.py
+++ KMeans/kmeans.py
@@ -1,7 +1,12 @@
 import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
 
 x = [4, 5, 10, 4, 3, 11, 14 , 6, 10, 12]
 y = [21, 19, 24, 17, 16, 25, 24, 22, 21, 21]
 
-plt.scatter(x, y)
+data = list(zip(x, y))
+kmeans = KMeans(n_clusters=2)
+kmeans.fit(data)
+
+plt.scatter(x, y, c=kmeans.labels_)
 plt.show()
