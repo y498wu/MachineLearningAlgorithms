bf79e571253ba78336b46cd3d81be45c4fee2f54
Yeshu Wu <y498wu@uwaterloo.ca>
2023-04-29 23:23:36 -0400

    create 4 clusters scatter

---
+++ KMeans/kmeans_clusters.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.datasets._samples_generator import make_blobs
+
+X, y_true = make_blobs(n_samples=300, centers=4,
+                       cluster_std=0.60, random_state=0)
+plt.scatter(X[:, 0], X[:, 1], s=50)
+plt.show()
