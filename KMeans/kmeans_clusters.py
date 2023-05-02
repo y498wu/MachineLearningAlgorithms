212aec9bc43a8991ff98346d05ec346e268f80c3
Yeshu Wu <y498wu@uwaterloo.ca>
2023-05-01 23:45:16 -0400

    kmeans implementation

--- KMeans/kmeans_clusters.py
+++ KMeans/kmeans_clusters.py
@@ -2,11 +2,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.datasets._samples_generator import make_blobs
 from sklearn.cluster import KMeans
+from sklearn.metrics import pairwise_distances_argmin
 
 X, y_true = make_blobs(n_samples=300, centers=4,
                        cluster_std=0.60, random_state=0)
 plt.scatter(X[:, 0], X[:, 1], s=50)
 
+'''
 kmeans = KMeans(n_clusters=4)
 kmeans.fit(X)
 y_kmeans = kmeans.predict(X)
@@ -15,4 +17,30 @@ plt.scatter(X[:, 0], X[:, 1], c=y_kmeans, s=50, cmap='viridis')
 
 centers = kmeans.cluster_centers_
 plt.scatter(centers[:, 0], centers[:, 1], c='black', s=200, alpha=0.5)
+plt.show()
+'''
+def find_clusters(X, n_clusters, rseed=2):
+    # 1. Randomly choose clusters
+    rng = np.random.RandomState(rseed)
+    i = rng.permutation(X.shape[0])[:n_clusters]
+    centers = X[i]
+    
+    while True:
+        # 2a. Assign labels based on closest center
+        labels = pairwise_distances_argmin(X, centers)
+        
+        # 2b. Find new centers from means of points
+        new_centers = np.array([X[labels == i].mean(0)
+                                for i in range(n_clusters)])
+        
+        # 2c. Check for convergence
+        if np.all(centers == new_centers):
+            break
+        centers = new_centers
+    
+    return centers, labels
+
+centers, labels = find_clusters(X, 4)
+plt.scatter(X[:, 0], X[:, 1], c=labels,
+            s=50, cmap='viridis');
 plt.show()
