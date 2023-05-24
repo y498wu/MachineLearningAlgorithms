71f9de1d3222b8e935040a733c2e8e391535610a
Yeshu Wu <y498wu@uwaterloo.ca>
2023-05-23 23:55:02 -0400

    explain torchvision-datasets.MNIST and transforms.ToTensor

--- LogisticRegression/logistic_pytorch.py
+++ LogisticRegression/logistic_pytorch.py
@@ -1,14 +1,16 @@
 import torch
-import torchvision.transforms as transforms
-from torchvision import datasets
-
 # torchvision.transforms: common image transformations.
 # They accept tensor images/batch of tensor images.
 # A Tensor Image is a tensor with (C, H, W) shape, where C is a number of channels, H and W are image height and width.
+import torchvision.transforms as transforms
+# Torchvision provides many built-in datasets in the torchvision.datasets module, 
+# as well as utility classes for building your own datasets.
+from torchvision import datasets
  
 # loading training data
 train_dataset = datasets.MNIST(root='./data', 
                                train=True, 
+                               # Convert a PIL Image or ndarray to tensor and scale the values accordingly.
                                transform=transforms.ToTensor(),
                                download=True)
 #loading test data
