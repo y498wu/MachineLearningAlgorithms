04c74bb0c5d0d28ba1c75f36beaae99b5fc4a191
Yeshu Wu <y498wu@uwaterloo.ca>
2023-05-24 23:32:13 -0400

    print the labels of the first two samples

--- LogisticRegression/logistic_pytorch.py
+++ LogisticRegression/logistic_pytorch.py
@@ -13,7 +13,7 @@ train_dataset = datasets.MNIST(root='./data',
                                # Convert a PIL Image or ndarray to tensor and scale the values accordingly.
                                transform=transforms.ToTensor(),
                                download=True)
-#loading test data
+# loading test data
 test_dataset = datasets.MNIST(root='./data', 
                               train=False, 
                               transform=transforms.ToTensor())
@@ -23,6 +23,9 @@ print("number of training samples: " + str(len(train_dataset)) + "\n" +
       "number of testing samples: " + str(len(test_dataset)))
 
 # inspect the data type and size of the first element in the training data
+# the first index [0] represents that this is the first element
+# the second index [0] represents that this is the image
+# Each sample in the dataset is a pair of image and label.
 print("datatype of the 1st training sample: ", train_dataset[0][0].type())
 print("size of the 1st training sample: ", train_dataset[0][0].size())
 
@@ -33,3 +36,8 @@ print("size of the 1st training sample: ", train_dataset[0][0].size())
 # The first sample in the dataset is a FloatTensor and it is a 
 # 28x28-pixel image in grayscale, hence the size [1, 28, 28].
 
+# print the labels of the first two samples in the training set.
+# the first index [0] and [1] represent that these are the first and second elements
+# the second index [1] represents that these are labels
+print("label of the first taining sample: ", train_dataset[0][1])
+print("label of the second taining sample: ", train_dataset[1][1])
